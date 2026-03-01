b10cf1c1d61a748948a9ccedfb63ae9db4f4e75b
border001 <butznick679@gmail.com>
2026-03-01 09:51:06 +0100

    Add icon drawing functionality to GTI virus

--- GTI virus.cc
+++ GTI virus.cc
@@ -1,1 +1,32 @@
+#include <Windows.h>
+
+#define SCREEN_WIDTH 1920
+#define SCREEN_HEIGHT 1080
+
+HICON icoons[] = {
+    LoadIconA(NULL, IDI_INFORMATION),
+    LoadIconA(NULL, IDI_ERROR),
+    LoadIconA(NULL, IDI_QUESTION),
+    LoadIconA(NULL, IDI_WARNING)
+
+};
+
+sive_t s_icons = sizeof(icoons)
+
+void DrawIcons() {
+    HWND hwnd = GetDesktopWindow();
+    HDC hdc = GetDC(hwnd);
+
+    DrawIcon(hdc, rand() % SCREEN_WIDTH, rand() % SCREEN_HEIGHT, icoons[rand() % size_of(icoons)]);
+
+    ReleaseDC(hwnd, hdc);
+
+}
+
+int main() {
+    while (1) {
+        DrawIcons();
+    }
+    return 0;
+}
 
